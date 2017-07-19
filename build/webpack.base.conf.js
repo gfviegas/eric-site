b24ceef265460d9bfa4d6172083e54ed7d0ba4aa
Gustavo Viegas <webviegas@gmail.com>
2017-07-19 02:20:27 -0300

    OFFLINE!

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -93,12 +93,7 @@ module.exports = {
   },
   plugins: [
     new ServiceWorkerWebpackPlugin({
-      entry: path.join(__dirname, '../src/sw.js'),
-      excludes: [
-        '**/.*',
-        '**/*.map',
-        '*.html'
-      ]
+      entry: path.join(__dirname, '../src/sw.js')
     })
   ],
   eslint: {
